8eee29838e59f3ef559b0bc3e74739e1928e9de4
Stargirl Flowers <thea@winterbloom.com>
2025-03-06 16:33:41 -0500

    Update nav.js to direct `Attend` to the updated Attend page.

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -1,6 +1,6 @@
 export default {
     items: [
-        { title: "Attend", url: "https://www.eventbrite.ca/e/1067611086499/?aff=oddtdtcreator" },
+        { title: "Attend", url: "/attend/" },
         // { title: "Speak", url: "https://docs.google.com/forms/d/e/1FAIpQLSeiuWtUClyQ2Aj-CBFwZacpfL9xxgG_ZA8ImF0SF9sT2-sqKA/viewform" },
         { title: "Schedule", url: "/schedule/" },
         { title: "Sponsor", url: "/sponsor/" },
