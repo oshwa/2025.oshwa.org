e4e734b79d9109e762fc2173e9944a858f8e19ab
Thea Flowers <thea@winterbloom.com>
2025-03-08 16:17:49 -0500

    Added sponsors

---
+++ src/_data/sponsors.js
@@ -0,0 +1,106 @@
+export default [
+    {
+        heading: "WiFi sponsor",
+        layout: "one-column",
+        sponsors: [
+            {
+                name: "Crowd Supply",
+                url: "https://crowdsupply.com",
+                image: "crowdsupply.webp",
+            },
+        ],
+    },
+    {
+        heading: "Refreshments sponsor",
+        layout: "one-column",
+        sponsors: [
+            {
+                name: "SparkFun",
+                url: "https://sparkfun.com",
+                image: "sparkfun.webp",
+            },
+        ],
+    },
+    {
+        heading: "Enthusiast sponsors",
+        layout: "two-columns",
+        sponsors: [
+            {
+                name: "Pro²Network",
+                url: "https://prosquared.org",
+                image: "pro2network.webp",
+            },
+            {
+                name: "Hackster",
+                url: "https://hackster.io",
+                image: "hackster.webp"
+            },
+        ],
+    },
+    {
+        heading: "Supporting sponsors",
+        layout: "three-columns",
+        sponsors: [
+            {
+                name: "Olimex",
+                url: "https://www.olimex.com",
+                image: "olimex.webp",
+            },
+            {
+                name: "PulseSensor",
+                url: "https://pulsesensor.com",
+                image: "pulsesensor.webp",
+            },
+            {
+                name: "Opulo",
+                url: "https://opulo.io",
+                image: "opulo.svg",
+            },
+            {
+                name: "ThingM",
+                url: "https://thingm.com",
+                image: "thingm.webp",
+            },
+        ],
+    },
+    {
+        heading: "Community sponsors",
+        layout: "four-columns",
+        sponsors: [
+            {
+                name: "WinSource",
+                url: "https://www.win-source.net",
+                image: "winsource.webp",
+            },
+            {
+                name: "SK Consulting",
+                url: "https://kazarians.dev/",
+                image: "sk-consulting.webp",
+            },
+            {
+                name: "Frame.work",
+                url: "https://frame.work",
+                image: "framework.webp",
+            },
+            {
+                name: "Adafruit",
+                url: "https://adafruit.com",
+                image: "adafruit.webp",
+            },
+            {
+                name: "LDO Motors",
+                url: "https://www.ldomotors.com/",
+                image: "ldo-motors.webp",
+            },
+            {
+                name: "Soldered",
+                url: "https://soldered.com/",
+                image: "soldered.webp",
+            },
+            {
+                name: "Varro Life Sciences",
+                url: "https://varrobio.com/",
+            },
+        ],
+    },
+];
